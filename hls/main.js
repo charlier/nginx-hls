25989572d3a59187cb30387f35be512c36283306
Charlie Rogers <charlier@users.noreply.github.com>
2019-04-12 16:49:27 +0100

    typo

--- hls/main.js
+++ hls/main.js
@@ -2,7 +2,7 @@ if(Hls.isSupported()) {
     const video = document.getElementById('video');
     const hls = new Hls();
     const stream = window.location.search.slice(1) || "playlist";
-    const.loadSource(`${stream}.m3u8`);
+    hls.loadSource(`${stream}.m3u8`);
     hls.attachMedia(video);
     hls.on(Hls.Events.MANIFEST_PARSED, function() {
         video.addEventListener("click", () => video.play());
